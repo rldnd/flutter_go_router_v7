2516d448a92b7feae010fb3d2931279dfc07d055
rldnd <gi981226@gmail.com>
2023-05-20 20:58:42 +0900

    feat: auth logic with goRouter

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_go_router_v7/layout/default_layout.dart';
+import 'package:flutter_go_router_v7/provider/auth_provider.dart';
 import 'package:flutter_go_router_v7/screen/third_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -28,7 +30,13 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               context.go('error');
             },
-            child: const Text('Screen THIRD (GO)'),
+            child: const Text('Screen ERROR (GO)'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(userProvider.notifier).logout();
+            },
+            child: const Text('LOGOUT'),
           ),
         ],
       ),
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_go_router_v7/layout/default_layout.dart';
+import 'package:flutter_go_router_v7/provider/auth_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
       body: Column(
-        children: [],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              ref.read(userProvider.notifier).login(name: 'ㅋㅋ');
+            },
+            child: Text('Login'),
+          ),
+        ],
       ),
     );
   }
